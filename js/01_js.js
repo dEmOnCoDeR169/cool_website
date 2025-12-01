fbd24fd0a3f4bd69432688e65e290b52ba3adcb3
dEmOnCoDeR169 <pradyumna1809@gmail.com>
2025-12-01 21:18:23 +0530

    lol

--- js/01_js.js
+++ js/01_js.js
@@ -1,7 +1,6 @@
 fetch("https://cool-website-back.onrender.com/")
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => {
-      console.log(data.message); 
       document.body.innerHTML += `<p>Backend says: ${data.message}</p>`;
   })
-  .catch(err => console.error("Error:", err));
+  .catch(err => console.error("Fetch error:", err));
